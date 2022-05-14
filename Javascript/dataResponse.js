ae5a5985ed7d05df360ffcb77b4f5bd2283a7c04
Nancy20m4c <nmmc20491@gmail.com>
2022-05-14 09:23:06 +0200

    cambios en configuracion

--- Javascript/dataResponse.js
+++ Javascript/dataResponse.js
@@ -1,5 +1,5 @@
 class Project {
-  constructor( projectCode, name, description, clientCode, contractCode, users, commits){
+  constructor(projectCode, name, description, clientCode, contractCode, users, commits, images) {
     this.projectCode = projectCode;
     this.name = name;
     this.description = description;
@@ -7,64 +7,181 @@ class Project {
     this.contractCode = contractCode;
     this.users = users;
     this.commits = commits;
+    this.images = images;
+  }
+
+  // funcion encargada de cargar los commits el codigo proyecto empieza por el
+  addCommit(commit) {
+    if (commit.CodeCommit.startsWith(this.projectCode)) {
+      this.commits.push(commit);
+
+    }
   }
 }
 
 class Table {
-    constructor(){
-       this.mainPanel = document.getElementById('mainPanel');
-       this.tableProjectTbody = document.querySelector('#panelProjects tbody');
-       this.tableProjectThead = document.querySelector('#panelProjects thead');
-       this.projectList = [];
-       this.commitList = null;
-       this.projectPanel = document.querySelector('#project-panel');
-       this.tableBody = document.querySelector('#tableBody');
+  constructor() {
+    this.mainPanel = document.getElementById('mainPanel');
+    this.tableProjectTbody = document.querySelector('#panelProjects tbody');
+    this.tableProjectThead = document.querySelector('#panelProjects thead');
+    this.projectList = [];
+    this.activeProject;
+    //this.commitList = [];
+    this.projectPanel = document.querySelector('#project-panel');
+    this.tableBody = document.querySelector('#tableBody');
 
-       this.loadData();
-       
-    }
+    this.loadData();
 
-    loadData(){
-      fetch('data/response01Projects.json')
+  }
+
+  loadData() {
+    // cargamos proyectos
+    fetch('data/response01Projects.json')
       .then(response => response.json())
       .then(dataProject => {
 
-      /*fetch('data/response02Commits.json')
-        .then(response => response.json())
-        .then(dataCommit => { 
-          console.log(dataCommit);*/
-      
-        dataProject.message.forEach( project => {
-            let card = document.createElement('mmc-card');
-            card.setAttribute("code", project.CodigoProyecto)
-            card.setAttribute("name", project.Nombre);
-            card.setAttribute("description", project.Descripcion);
-            card.setAttribute("code", project.CodigoProyecto);
-            
-            card.addEventListener("click", function(){
-              _onClickCard(this, project);
-            });
-            this.projectPanel.append(card);
-          });
+        // recorremos los proyectos
+        dataProject.message.forEach(project => {
+
+          // utilizo mi clase project para guardar los datos que me interesanm de cada proyecto.
+          const myProject = new Project(
+            project.CodigoProyecto,
+            project.Nombre,
+            project.Descripcion,
+            project.CodigoCliente,
+            project.CodigoContratista,
+            project.Users,
+            [],
+            project.PathLogos
+          );
+
+          // cargo cada proyecto al listado de mi clase Table
+          this.projectList.push(myProject);
+
+          // creamos un componente card y seteamos sus atributos
+          let card = this.addCardToProjectPanelHTML(myProject);
+          let tr = this.addProjectTrToTableByIdHTML(myProject, 'tableProject');
+
+        });
       });
 
-      function _onClickCard(element, project) {
-        console.log(project);
-        console.log(element);
-        fetch('data/response02Commits.json')
-        .then(response => response.json())
-        .then(dataCommit => { 
-          dataCommit.message.forEach(commit => {
-            if(commit.CodeCommit.startsWith(element.CodigoProyecto)) {
-              this.commitList.push(commit);
-              
-              console.log(this.commitList);
-             }
-          }); 
-          console.log(commitList);
+  };
+
+  removeOtherProjectTr(trProject, tableId) {
+    const query = '#' + tableId + ' tbody tr';
+
+    const trList = document.querySelectorAll(query);
+
+    trList.forEach(tr => {
+      if (trProject != tr) {
+         tr.remove();
+      }
+    });
+
+  }
+
+  addProjectTrToTableByIdHTML(project, tableId) {
+    const query = '#' + tableId + ' tbody';
+    console.log(query);
+    const tableBody = document.querySelector(query);
+
+    const tr = document.createElement('tr');
+
+    const tdLogo = document.createElement('td');
+    tdLogo.innerHTML = `<img style="zoom: .3;" src="images\\descarga.png">`;
+    tr.append(tdLogo);
+
+    const tdCode = document.createElement('td');
+    tdCode.innerText = project.projectCode;
+    tr.append(tdCode);
+
+    const tdName = document.createElement('td');
+    tdName.innerText = project.name;
+    tr.append(tdName);
+
+    const tdDesc = document.createElement('td');
+    tdDesc.innerText = project.description;
+    tr.append(tdDesc);
+
+    /*     const tdActions = document.createElement('td');
+        tdActions.innerHTML = `
+          <i class="fa fa-plus" aria-hidden="true"></i>
+          <i class="fa fa-pencil-square-o" aria-hidden="true"></i>  
+          <i class="fa fa-trash-o" aria-hidden="true"></i>
+        `;
+        console.log(tdActions);
+        tr.append(tdActions); */
+
+    tr.addEventListener(
+      'click', function () {
+        table.removeOtherProjectTr(this, 'tableProject');
       });
+
+    tr.addEventListener(
+      'mouseenter', function (event) {
+
+        // eliminamos todos los action panels
+        const actionPanels = document.querySelectorAll('.actionPanel');
+        actionPanels.forEach(a => {
+          a.remove();
+        });
+
+        // añadimos un action panel al ultimo TD del TR activo
+        const lastTd = this.children[this.children.length - 1];
+        lastTd.insertAdjacentHTML('afterend', `
+          <div class="actionPanel">
+            <div style="flex: 1 1 auto;">...</div>
+            <i style="flex: 1 1 auto;" class="fa fa-plus" aria-hidden="true"></i>
+            <i style="flex: 1 1 auto;" class="fa fa-pencil-square-o" aria-hidden="true"></i>  
+            <i style="flex: 1 1 auto;" class="fa fa-trash-o" aria-hidden="true"></i>
+          </div> 
+          `);
+      });
+
+    tableBody.append(tr);
+
+    return tr;
+  }
+
+  addCardToProjectPanelHTML(project) {
+    let card = document.createElement('mmc-card');
+    card.setAttribute("code", project.projectCode);
+    card.setAttribute("name", project.name);
+    card.setAttribute("description", project.description);
+
+    //anyado el card al panel principal de proyectos
+    this.projectPanel.append(card);
+
+    // crep un evento click para cada compomente card.
+    card.addEventListener("click", function () {
+      _onClickCard(this, project);
+    });
+
+    function _onClickCard(element, project) {
+      table.activeProject = project;
+      table.activeProject.commits = [];
+
+
+      fetch('data/response02Commits.json')
+        .then(response => response.json())
+        .then(dataCommit => {
+          dataCommit.message.forEach(commit => {
+            table.activeProject.addCommit(commit);
+          });
+        });
+
+      console.log(table.activeProject);
     }
-    
+  }
+
+}
+const table = new Table();
+
+
+
+
+
+
 
 /* 
     loadCommit(projectCode) {
@@ -126,7 +243,3 @@ class Table {
 
       tableTbody.append(tr);
     }  */
-
-};
-}
-const table = new Table();
